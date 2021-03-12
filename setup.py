b3737d1ac56bcab5b416db8484b4253afdc61d69
nitipol012 <63086354+nitipol012@users.noreply.github.com>
2021-03-12 18:12:13 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Nitipol & Kantitat',                   # Type in your name
   author_email = 'aunitipol@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/nitipol012/GoCar',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/nitipol012/GoCar/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/nitipol012/GoCar/archive/v_02.tar.gz',    # I explain this later on
   keywords = ['RaspberryPi', 'GoCar'],   # Keywords that define your package best
   install_requires=['numpy'],          # I get to this in a second
  
